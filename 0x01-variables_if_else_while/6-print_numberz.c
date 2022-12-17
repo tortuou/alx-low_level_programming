a42fb02fa6646f34f0a72fbb362a39e67289a6ad
tortuou <totajeuses@gmail.com>
2022-12-17 07:53:12 -0800

    t6u

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,39 +1,20 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
+	int d;
 
-		int d;
-
-
-
-			for (d = '0'; d <= '9'; d++)
-
-						putchar(d);
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
 }
