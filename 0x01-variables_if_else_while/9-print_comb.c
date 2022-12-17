e182bec2fcac449db15c5438252fc7fcf041827c
tortuou <totajeuses@gmail.com>
2022-12-17 08:07:07 -0800

    t9u

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,53 +1,26 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
+	int d;
 
-		int d;
-
-
-
-			for (d = '0'; d <= '9'; d++)
-
-					{
-
-								putchar(d);
-
-										if (d != '9')
-
-													{
-
-																	putchar(',');
-
-																				putchar(' ');
-
-																						}
-
-											}
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',')
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
 }
