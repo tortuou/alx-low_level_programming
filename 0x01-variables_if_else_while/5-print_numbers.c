8420078391b345914a6dbbd09aac20d64d7d192b
tortuou <totajeuses@gmail.com>
2022-12-17 07:48:24 -0800

    t5u

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,39 +1,19 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
+	int i;
 
-		int i;
-
-
-
-			for (i = 0; i < 10; i++)
-
-						printf("%d", i);
-
-				printf("\n");
-
-
-
-					return (0);
-
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
 }
