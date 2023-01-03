3013cb10be21e150ae7878ad34ff2f03ef7e4441
tortuou <totajeuses@gmail.com>
2023-01-02 17:36:54 -0800

    t0u

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,11 @@
  */
 char *_memset(char *s, char b, unsigned int n);
 {
-	unsigned int i = 0;
-
-	while (i < n)
+	int i = 0;
+	for(;n > 0 ; i++)
 	{
 		s[i] = b;
-		i++;
+		n--;
 	}
 	return (s);
 }
