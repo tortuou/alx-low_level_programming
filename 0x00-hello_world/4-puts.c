6d43afb1844a5e3c28e88966cf89614afbb3bdd4
tortuou <totajeuses@gmail.com>
2023-08-13 11:08:42 -0700

    prints using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  *
- * Return : 0
+ * Return: 0 value
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
