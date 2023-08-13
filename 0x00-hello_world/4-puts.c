ccc44faf92b739c9f00d7a9ce7448555e83a54a2
tortuou <totajeuses@gmail.com>
2023-08-13 10:58:26 -0700

    prints using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*prints exactly "Programming is like building a multilingual puzzle
+/*
+ * prints exactly "Programming is like building a multilingual puzzle
  */
-
 int main(){
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
