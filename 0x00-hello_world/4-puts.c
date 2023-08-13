16de7063f9e226f3a661fdc41367a80938de5e61
tortuou <totajeuses@gmail.com>
2023-08-13 11:01:01 -0700

    prints using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /*
+ * main - entry point
+ *
  * prints exactly "Programming is like building a multilingual puzzle
+ *
+ * Return : 0
  */
 int main(){
 	puts("\"Programming is like building a multilingual puzzle");
