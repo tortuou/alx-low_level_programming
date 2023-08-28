2c6d7f3f6aec1fed8044e2e120098d399ac1df1c
tortuou <totajeuses@gmail.com>
2023-08-28 14:56:48 -0700

    prints all single digit numbers of base 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-        int n;
+	int n;
 
-        for (n = '0'; n <= '9'; n++)
-        {
-                putchar (n);
-        }
-        putchar ('\n');
-        return (0);
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar (n);
+	}
+	putchar ('\n');
+	return (0);
 }
