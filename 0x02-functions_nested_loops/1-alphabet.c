58081f6eb1fe2b0e4d11b157be09ebd80a9af049
tortuou <totajeuses@gmail.com>
2023-08-31 11:50:48 -0700

    prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
