fc15b366391ebbcee16fec6154111ec670098567
tortuou <totajeuses@gmail.com>
2023-10-24 04:27:06 -0700

    creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	ptr = malloc(sizeof(*ptr) * size);
-	if (size != 0 || ptr != NULL)
+	if (ptr == NULL)
+		return (NULL);
+	if (size != 0)
 	{
 		for (i = 0; i < size; i++)
 			ptr[i] = c;
