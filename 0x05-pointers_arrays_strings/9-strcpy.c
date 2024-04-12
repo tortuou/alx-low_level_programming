49d60a7de224e99254ed72bd1207f5cda55ced1a
tortuou <totajeuses@gmail.com>
2024-04-12 13:01:13 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * *_strcpy- function that copies the string src to buffer dest
+ * @*dest: buffer
+ * @*src: string
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
